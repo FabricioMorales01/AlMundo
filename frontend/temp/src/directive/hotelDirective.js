10aff3e6e66799cc4561763cb380974a727d5813
FabricioMorales01 <41924757+FabricioMorales01@users.noreply.github.com>
2018-07-30 14:21:29 -0500

    Revert "Initial commit"
    This reverts commit e37935c430cd1f33cca774fec9a3cf8c68236347.

--- frontend/temp/src/directive/hotelDirective.js
+++
@@ -1,1 +0,0 @@
-(function(app){var HotelController=function(){function HotelController($scope){this.$scope=$scope}return HotelController}();app.directive("amHotel",[function($parse){return{restrict:"E",templateUrl:"src/view/directive/hotel.html",scope:{hotel:"="},controller:HotelController,controllerAs:"ctrl",bindToController:true}}])})(angular.module("app"));
